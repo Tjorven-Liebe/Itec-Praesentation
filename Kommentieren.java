5b63e7df043987c886cd07b57e6e33a12ece9859
Tjorven Liebe <32434395+Tjorven-Liebe@users.noreply.github.com>
2022-09-21 10:40:23 +0200

    Update Kommentieren.java

--- Kommentieren.java
+++ Kommentieren.java
@@ -1,31 +1,33 @@
-/**
-* Hier wird das imperative Programm gestartet
-* 
-* Das imperative Programm extendet auf der Hamster.java Klasse
-* also kann man direkt auf ihre Methoden zugreifen.
-* @return void
-*/
-void main() {
-	rechtsUm();
-	kehrt();
-}
+class Unnamed {
+	/**
+	* Hier wird das imperative Programm gestartet
+	* 
+	* Das imperative Programm extendet auf der Hamster.java Klasse
+	* also kann man direkt auf ihre Methoden zugreifen.
+	* @return void
+	*/
+	void main() {
+		rechtsUm();
+		kehrt();
+	}
 
-/**
-* RechtsUm
-* dreht den hamster drei mal nach links, damit dieser nach rechts schaut
-* @return void
-*/
-void rechtsUm() {
-	linksUm();
-	linksUm();
-}
+	/**
+	* RechtsUm
+	* dreht den hamster drei mal nach links, damit dieser nach rechts schaut
+	* @return void
+	*/
+	void rechtsUm() {
+		linksUm();
+		linksUm();
+	}
 
-/**
-* kehrt
-* dreht den Hamster zwei mal nach links damit er sich um 180° gedreht hat
-* @return void
-*/
-void kehrt() {
-	linksUm();
-	linksUm();
+	/**
+	* kehrt
+	* dreht den Hamster zwei mal nach links damit er sich um 180° gedreht hat
+	* @return void
+	*/
+	void kehrt() {
+		linksUm();
+		linksUm();
+	}
 }
